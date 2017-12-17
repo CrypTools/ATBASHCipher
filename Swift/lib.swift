af6d9c98a3dd2ba54578757fe218c4981d7a8b7d
Arthur Guiot <arguiot@gmail.com>
2017-12-17 17:19:59 +0800

    Added Swift extension and modified JS files

---
+++ Swift/lib.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+extension String {
+    var encrypt : String {
+        let alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, ".components(separatedBy: ",")
+        var output = ""
+        var i = Int()
+        for i in self {
+            var indexed = 0;
+            if let index = alphabet.index(of: String(i).lowercased()) {
+                indexed = index;
+            }
+            output += alphabet[26 - indexed]
+        }
+        return output
+    }
+    var decrypt : String {
+        return self.encrypt
+    }
+}
