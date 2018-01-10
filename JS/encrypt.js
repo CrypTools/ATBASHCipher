b7552777be7f49e26b87ec5dd379ac6a89236044
Arthur Guiot <arguiot@gmail.com>
2018-01-10 19:42:26 +0800

    Added tests

--- JS/encrypt.js
+++ JS/encrypt.js
@@ -2,7 +2,8 @@ String.prototype.encrypt = function() {
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz '.split('')
 	let output = "";
 	for (let i of this) {
-		output += alphabet[26 - alphabet.indexOf(i.toLowerCase())]
+		output += alphabet[25 - alphabet.indexOf(i.toLowerCase())]
 	}
 	return output;
 }
+module.exports = text => text.encrypt()
---
+++ __test__/test.js
@@ -0,0 +1,15 @@
+// Test made using EyeJS - https://eye.js.org
+
+const path = require('path').normalize(__testDir + "/../JS/")
+
+const encrypt = require(path + "encrypt.js")
+const decrypt = require(path + "decrypt.js")
+
+
+eye.test("Encryption", "node",
+	$ => $(encrypt("attack")).Equal("zggzxp")
+)
+eye.test("Decryption", "node",
+	$ => $(decrypt("zggzxp")).Equal("attack"),
+	$ => $(decrypt(encrypt("attack"))).Equal("attack")
+)
