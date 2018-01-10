917b59a9b6393b06d48e012acf3ff452b5b63294
Arthur Guiot <arguiot@gmail.com>
2018-01-10 19:47:34 +0800

    Removed warning from the Swift compiler

--- Swift/lib.swift
+++ Swift/lib.swift
@@ -10,7 +10,6 @@ extension String {
     var encrypt : String {
         let alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, ".components(separatedBy: ",")
         var output = ""
-        var i = Int()
         for i in self {
             var indexed = 0;
             if let index = alphabet.index(of: String(i).lowercased()) {
